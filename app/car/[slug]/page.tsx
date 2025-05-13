2e6a85f391b6d263148a811a105e95008ad1dbe9
rezaeiar <rezaeiar0@gmail.com>
2025-05-13 18:14:45 +0330

    feat: develop car details

---
+++ app/car/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import ProductOverview from '@/app/components/car/ProductOverview'
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <ProductOverview />
+        </>
+    )
+}
+
+export default page
---
+++ app/components/car/ProductOverview.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import BannerPrimary from '../home/HeroBanners/banners/BannerPrimary'
+import Image from 'next/image'
+
+const ProductOverview = () => {
+    return (
+        <div className="">
+            <div className="container mx-auto">
+                <div className='grid grid-cols-3 gap-6'>
+                    <div className="space-y-6">
+                        <BannerPrimary />
+                        <div className="grid grid-cols-3 gap-6">
+                            <div className="">
+                                <Image
+                                    height={360}
+                                    width={640}
+                                    alt='BG'
+                                    src={"/images/cars/View 2.png"}
+                                    className='w-full object-cove'
+                                />
+                            </div>
+                            <div className="">
+                                <Image
+                                    height={360}
+                                    width={640}
+                                    alt='BG'
+                                    src={"/images/cars/View 2.png"}
+                                    className='w-full object-cove'
+                                />
+                            </div>
+                            <div className="">
+                                <Image
+                                    height={360}
+                                    width={640}
+                                    alt='BG'
+                                    src={"/images/cars/View 2.png"}
+                                    className='w-full object-cove'
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-span-2 p-6 bg-white shadow rounded-xl">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductOverview
