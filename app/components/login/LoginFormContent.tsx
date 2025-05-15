9254699b8f82574bc372c6a19848f76622d98a51
rezaeiar <rezaeiar0@gmail.com>
2025-05-15 23:34:28 +0330

    feat: add login form

---
+++ app/components/login/LoginFormContent.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+import Logo from '../common/Logo'
+import LoginForm from './LoginForm'
+
+const LoginFormContent = () => {
+    return (
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 h-screen'>
+            <div className="h-screen lg:col-span-2 hidden md:block">
+                <Image
+                    height={2000}
+                    width={3000}
+                    alt='bg'
+                    src={"/images/bg.jpg"}
+                    className='h-full object-cover w-full'
+                />
+            </div>
+            <div className="bg-white p-6 md:p-12 space-y-12 flex flex-col h-screen overflow-y-auto">
+                <Logo
+                    title='morent'
+                />
+                <LoginForm />
+            </div>
+        </div>
+    )
+}
+
+export default LoginFormContent
