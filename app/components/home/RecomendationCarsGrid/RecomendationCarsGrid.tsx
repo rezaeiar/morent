e54b20614c7a76a1dac75df49c205eedaf2a8a99
rezaeiar <rezaeiar0@gmail.com>
2025-05-16 11:23:12 +0330

    feat: responsive recomended section

--- app/components/home/RecomendationCarsGrid/RecomendationCarsGrid.tsx
+++ app/components/home/RecomendationCarsGrid/RecomendationCarsGrid.tsx
@@ -160,7 +160,7 @@ const RecomendationCarsGrid = () => {
               Recomendation Car
             </h3>
           </div>
-          <div className="grid grid-cols-4 gap-8">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
             {
               cars.map((car) => (
                 <Card {...car} key={car.id} />
