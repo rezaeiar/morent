24e95c9278f8702a65702e8e5ae64ecff66eea2f
rezaeiar <rezaeiar0@gmail.com>
2025-05-16 11:29:12 +0330

    feat: create popular section

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,12 @@
 import HeroBanners from "../components/home/HeroBanners/HeroBanners";
+import PopularCarsGrid from "../components/home/PopularCarsGrid/PopularCarsGrid";
 import RecomendationCarsGrid from "../components/home/RecomendationCarsGrid/RecomendationCarsGrid";
 
 export default function Home() {
     return (
         <>
             <HeroBanners />
+            <PopularCarsGrid />
             <RecomendationCarsGrid />
         </>
     );
---
+++ app/components/home/PopularCarsGrid/PopularCarsGrid.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import Card from '../../common/Card/Card';
+
+type CardProps = {
+  id: number,
+  name: string;
+  type: string;
+  image: string;
+  transmission: "Manual" | "Automatic";
+  tankCapacity: number;
+  seats: number;
+  pricePerDay: number;
+  discountedPricePerDay: number | null;
+};
+
+const PopularCarsGrid = () => {
+
+  const cars: CardProps[] = [
+    {
+      id: 1,
+      name: "Toyota RAV4",
+      type: "SUV",
+      image: "/images/cars/Car.png",
+      transmission: "Automatic",
+      tankCapacity: 55,
+      seats: 5,
+      pricePerDay: 1200000,
+      discountedPricePerDay: 980000,
+    },
+    {
+      id: 2,
+      name: "Kia Sportage",
+      type: "SUV",
+      image: "/images/cars/Car.png",
+      transmission: "Automatic",
+      tankCapacity: 58,
+      seats: 5,
+      pricePerDay: 1150000,
+      discountedPricePerDay: 990000,
+    },
+    {
+      id: 3,
+      name: "Peugeot 206",
+      type: "Hatchback",
+      image: "/images/cars/Car.png",
+      transmission: "Manual",
+      tankCapacity: 50,
+      seats: 5,
+      pricePerDay: 450000,
+      discountedPricePerDay: 400000,
+    },
+    {
+      id: 4,
+      name: "Hyundai Elantra",
+      type: "Sedan",
+      image: "/images/cars/Car.png",
+      transmission: "Automatic",
+      tankCapacity: 53,
+      seats: 5,
+      pricePerDay: 980000,
+      discountedPricePerDay: 870000,
+    }
+  ];
+
+
+  return (
+    <div className='mb-10'>
+      <div className="container px-4 mx-auto">
+        <div className="space-y-5">
+          <div className="">
+            <h3 className='text-secondary-300'>
+              Popular Car
+            </h3>
+          </div>
+          <div className="grid lg:grid-cols-2 xl:grid-cols-4 gap-8">
+            {
+              cars.map((car) => (
+                <Card {...car} key={car.id} />
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PopularCarsGrid
